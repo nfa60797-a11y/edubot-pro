118a49884e27ecbac1f9d5173981e06e12adcf6f
nfa60797-a11y <nfa60797@gmail.com>
2026-05-31 15:00:04 +0700

    Create Main.jsx

---
+++ Src/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EduBotPro from './EduBotPro.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <EduBotPro />
+  </React.StrictMode>,
+)
